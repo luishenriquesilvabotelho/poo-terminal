ea28dfca334a1b3b1b077b1196ebda742195264a
Gustavo <vdanielly683@gmail.com>
2023-10-31 20:09:19 -0400

    modified:   funcoes.py 	modified:   main.py

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,5 @@
 from classes import *
+import os
 
 def cadastrar_aluno(nomec, sexoc, matriculac, senhac, turmac):
     aluno = Aluno(nomec, sexoc, matriculac, senhac, turmac)
@@ -6,3 +7,10 @@ def cadastrar_aluno(nomec, sexoc, matriculac, senhac, turmac):
 def cadastrar_professor(nome_c, sexo_c, matricula_c, senha_c):
     professor = Professor(nome_c, sexo_c, matricula_c, senha_c)
     professor.Cadastrar()
+
+def limpar_terminal():
+    os.system('cls')
+
+def login(matricula_login_aluno,senha_login_aluno, tipo_usuario):
+    usuario  = Usuario(None, None, matricula_login_aluno, senha_login_aluno)
+    usuario.Login(tipo_usuario)
