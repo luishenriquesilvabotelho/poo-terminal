f9892f9888317cdc8a191b31f04a4c2f5e88fd42
Gustavo <vdanielly683@gmail.com>
2023-11-04 15:37:55 -0400

    modified:   __pycache__/classes.cpython-311.pyc 	modified:   __pycache__/funcoes.cpython-311.pyc 	modified:   banco_de_dados.db 	modified:   classes.py 	modified:   main.py

--- classes.py
+++ classes.py
@@ -1,90 +1,65 @@
+
 import sqlite3
 
-
-
 class Usuario:
-    def __init__(self,nome,sexo,matricula,senha):
-
+    def __init__(self, nome, sexo, matricula, senha):
         self.nome = nome
         self.sexo = sexo
         self.matricula = matricula
         self.senha = senha
 
-    def Cadastrar(self, usuario, turma):
+    def Cadastrar(self, usuario, turma=None):
         with sqlite3.connect("banco_de_dados.db") as banco:
             cursor = banco.cursor()
             if usuario == "Aluno":
                 cursor.execute("INSERT INTO Aluno VALUES (?, ?, ?, ?, ?)", (
                     self.nome, self.matricula, self.senha, self.sexo, turma))
                 banco.commit()
+                print("Aluno cadastrado com sucesso")
 
             elif usuario == "Professor":
                 cursor.execute("INSERT INTO Professor (nome, matricula, senha, sexo) VALUES (?, ?, ?, ?)",
-                            (self.nome, self.matricula, self.senha, self.sexo))
+                               (self.nome, self.matricula, self.senha, self.sexo))
                 banco.commit()
+                print("Professor cadastrado com sucesso")
 
     def Login(self, usuario):
         with sqlite3.connect("banco_de_dados.db") as banco:
             cursor = banco.cursor()
-            if usuario == 1:
-                cursor.execute(
-                "SELECT * FROM Aluno WHERE (matricula == ? AND senha == ?)", (self.matricula, self.senha))
+            if usuario == "Aluno":
+                cursor.execute("SELECT * FROM Aluno WHERE matricula = ? AND senha = ?",
+                               (self.matricula, self.senha))
                 verificador = cursor.fetchall()
                 if len(verificador) > 0:
-                    print("Aluno Logado com sucessp")
+                    print("Aluno logado com sucesso")
                     return True
                 else:
-                    print("Alguma coisa estava incorreta!")
+                    print("Matrícula ou senha incorretas!")
                     return False
-            elif usuario == 'Professor':
-                cursor.execute(
-                "SELECT * FROM Professor WHERE (matricula == ? AND senha == ?)", (self.matricula, self.senha))
+            elif usuario == "Professor":
+                cursor.execute("SELECT * FROM Professor WHERE matricula = ? AND senha = ?",
+                               (self.matricula, self.senha))
                 verificador = cursor.fetchall()
                 if len(verificador) > 0:
-                    print("Prof Logado com sucessor")
+                    print("Professor logado com sucesso")
                     return True
                 else:
-                    print("Alguma coisa estava incorreta!")
+                    print("Matrícula ou senha incorretas!")
                     return False
 
 
 class Aluno(Usuario):
-    turmas = {
-        1: '1 INFO A',
-        2: '1 INFO B',
-        3: '1 ELET A',
-        4: '1 ELET B',
-        5: '1 EDIF A',
-        6: '1 EDIF B',
-        7: '1 QUIM A',
-        8: '1 QUIM B',
-        9: ' 2 INFO M',
-        10: '2 INFO V',
-        11: '2 ELET M',
-        12: '2 ELET V',
-        13: '2 EDIF M',
-        14: '2 EDIF V',
-        15: '2 QUIM M',
-        16: '2 QUIM V',
-        17: '3 INFO M',
-        18: '3 INFO V',
-        19: '3 ELET M',
-        20: '3 ELET V',
-        21: '3 EDIF M',
-        22: '3 EDIF V',
-        23: '3 QUIM M',
-        24: '3 QUIM V',
-    }
-    def __init__(self, nome, sexo, matricula, senha, num_turma):
-        super().__init__(nome, sexo, matricula, senha)
-        self.num_turma = num_turma
 
+    def __init__(self, nome, sexo, matricula, senha, turma):
+        super().__init__(nome, sexo, matricula, senha)
+        self.turma = turma
+        
     def Cadastrar(self):
-        turma = Aluno.turmas.get(self.num_turma,f'Turma {self.num_turma} não encontrada')
-        super().Cadastrar("Aluno", self.num_turma)
+        super().Cadastrar("Aluno", self.turma)
 
     def ExibirAlunos(self):
-        print('a')
+        print('Exibindo alunos')
+
 
 
 class Professor(Usuario):
@@ -92,11 +67,8 @@ class Professor(Usuario):
         super().__init__(nome, sexo, matricula, senha)
 
     def Cadastrar(self):
-        return super().Cadastrar("Professor", None)
-    
-    
+        return super().Cadastrar("Professor")
+
     def EditarInscricoes(self):
-        print("oi")
-
-
+        print("Editando inscrições")
 
--- main.py
+++ main.py
@@ -2,10 +2,8 @@ from funcoes import *
 from classes import *
 import os
 
-
-
 limpar_terminal()
-print('Seja bem vindo ao nosso sistema\nEscolha umas das opções abaixo')
+print('Seja bem-vindo ao nosso sistema\nEscolha uma das opções abaixo')
 
 login_ou_cadastro = int(input('[1] - Login\n[2] - Cadastro\n'))
 limpar_terminal()
@@ -13,48 +11,83 @@ while login_ou_cadastro != 1 and login_ou_cadastro != 2:
     limpar_terminal()
     print('Você digitou algo incorretamente')
     login_ou_cadastro = int(input('[1] - Login\n[2] - Cadastro\n'))
-    
-if login_ou_cadastro == 1: #LOGIN
+
+if login_ou_cadastro == 1:  # LOGIN
     print('Você irá Logar como?')
     aluno_ou_prof = int(input('[1] - Aluno\n[2] - Professor\n'))
-    
+
     while aluno_ou_prof != 1 and aluno_ou_prof != 2:
         limpar_terminal()
         print('Você digitou algo incorretamente')
         aluno_ou_prof = int(input('[1] - Aluno\n[2] - Professor'))
-        
-    if aluno_ou_prof == 1: # LOGIN -> ALUNO
+
+    if aluno_ou_prof == 1:  # LOGIN -> ALUNO
         limpar_terminal()
         print('Insira seus dados')
-        input_mat_login_aluno = int(input('Matricula: '))
+        input_mat_login_aluno = int(input('Matrícula: '))
         input_senha_login_aluno = input('Senha: ')
         login(input_mat_login_aluno, input_senha_login_aluno, aluno_ou_prof)
-        
-    if aluno_ou_prof == 2: # LOGIN -> PROFESSOR
+
+    if aluno_ou_prof == 2:  # LOGIN -> PROFESSOR
         limpar_terminal()
         print('Insira seus dados')
-        input_mat_login_prof = int(input('Matricula: '))
+        input_mat_login_prof = int(input('Matrícula: '))
         input_senha_login_prof = input('Senha: ')
         login(input_mat_login_prof, input_senha_login_prof, aluno_ou_prof)
 
-elif login_ou_cadastro == 2: #CADASTRO
+elif login_ou_cadastro == 2:  # CADASTRO
     aluno_ou_prof = int(input('[1] - Aluno\n[2] - Professor'))
     if aluno_ou_prof == 1:
         limpar_terminal()
-        print("Bem vindo(a)")
+        print("Bem-vindo(a)")
         nome_aluno = input("Digite seu nome: ")
         sexo_aluno = input("Digite seu sexo: ")
         matricula_aluno = int(input("Digite sua matrícula: "))
         senha_aluno = input("Digite sua senha: ")
-        turma_aluno = input("Digite sua turma: ")
-        cadastrar_aluno(nome_aluno, sexo_aluno, matricula_aluno, senha_aluno, turma_aluno)
+
+        # Dicionário de turmas
+        turmas = {
+            1: '1 INFO A',
+            2: '1 INFO B',
+            3: '1 ELET A',
+            4: '1 ELET B',
+            5: '1 EDIF A',
+            6: '1 EDIF B',
+            7: '1 QUIM A',
+            8: '1 QUIM B',
+            9: ' 2 INFO M',
+            10: '2 INFO V',
+            11: '2 ELET M',
+            12: '2 ELET V',
+            13: '2 EDIF M',
+            14: '2 EDIF V',
+            15: '2 QUIM M',
+            16: '2 QUIM V',
+            17: '3 INFO M',
+            18: '3 INFO V',
+            19: '3 ELET M',
+            20: '3 ELET V',
+            21: '3 EDIF M',
+            22: '3 EDIF V',
+            23: '3 QUIM M',
+            24: '3 QUIM V',
+        }
+        print("Turmas disponíveis:")
+        for key, value in turmas.items():
+            print(f"{key} - {value}")
+
+        num_turma_aluno = int(input("Escolha o número da turma desejada: "))
+        if num_turma_aluno in turmas:
+            turma_aluno = turmas[num_turma_aluno]
+            cadastrar_aluno(nome_aluno, sexo_aluno, matricula_aluno, senha_aluno, turma_aluno)
+        else:
+            print("Turma não encontrada. O aluno não foi cadastrado.")
+        
     else:
         limpar_terminal()
-        print("Bem vindo(a)")
+        print("Bem-vindo(a)")
         nome_prof = input("Digite seu nome: ")
         sexo_prof = input("Digite seu sexo: ")
         matricula_prof = int(input("Digite sua matrícula: "))
         senha_prof = input("Digite sua senha: ")
         cadastrar_professor(nome_prof, sexo_prof, matricula_prof, senha_prof)
-
-    
